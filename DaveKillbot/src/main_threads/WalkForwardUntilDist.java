fb2c4e899c1e07650394361b2a496d694cdf4260
npoCaputo <nicholasCaputo12@Augustana.edu>
2016-02-13 19:12:50 -0600

    Updated with debug methods, a countdown method, utility write() methods and more accurate distance measurements to base our readings from.

--- DaveKillbot/src/main_threads/WalkForwardUntilDist.java
+++ DaveKillbot/src/main_threads/WalkForwardUntilDist.java
@@ -1,8 +1,6 @@
 package main_threads;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import lejos.hardware.BrickFinder;
@@ -15,12 +13,12 @@ import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 
 public class WalkForwardUntilDist {
 
-	private static final double CELL_DISTANCE = 500;
+	private static final double CELL_DISTANCE = 50;
+	private static final double CHECK_DISTANCE = .5;
 
 	private static int sWidth;
 	private static int sHeight;
@@ -29,13 +27,11 @@ public class WalkForwardUntilDist {
 	private static DifferentialPilot robot;
 	private static final double WHEEL_DIAMETER = 5.4;
 	private static final double TRACK_WIDTH = 14.5;
-	private static final double TRAVEL_SPEED = 60;
+	private static final double TRAVEL_SPEED = 30;
 	private static final double ROTATE_SPEED = 60;
 	private static final RegulatedMotor LEFT_MOTOR = Motor.C;
 	private static final RegulatedMotor RIGHT_MOTOR = Motor.B;
 	private static final RegulatedMotor TOP_MOTOR = Motor.A;
-
-	private static EV3TouchSensor bumper;
 
 	private static EV3ColorSensor colorSense;
 
@@ -48,15 +44,46 @@ public class WalkForwardUntilDist {
 		WalkForwardUntilDist run = new WalkForwardUntilDist();
 
 		run.main.start();
-		
-		run.stopper.start();
 
 	}
 
-	private void solve(boolean testing) {
+	/**
+	 * Starts the program when the user presses a button.
+	 * 
+	 * @param testing,
+	 *            whether to only perform one action before stopping or continue
+	 *            until the maze is solved.
+	 */
+	private void startProgram(boolean debug) {
+		write("Press any button to start.");
+
+		Button.waitForAnyPress();
+
+		stopper.start();
+
+		try {
+			showCountdown(5, (short) 1000);
+		} catch (InterruptedException err) {
+			write(err.getMessage());
+		}
+
+		if (!debug) {
+			solve();
+		} else {
+			debug_checkDistances();
+		}
+	}
+
+	/**
+	 * Attempts to solve the maze with a left-forward-right-back rule.
+	 * 
+	 * @param testing,
+	 *            whether we are only doing one test
+	 */
+	private void solve() {
 		float[] distances = new float[3];
 
-		while (!atGoal(6) && !testing && !Button.ENTER.isDown()) {
+		while (!atGoal(6)) {
 			// checks left
 			neckLeft90();
 			distances[0] = getDistanceMeasurement();
@@ -79,12 +106,12 @@ public class WalkForwardUntilDist {
 			 * Goes left if possible, otherwise forward, otherwise right,
 			 * otherwise backwards
 			 */
-			if (distances[0] >= 1.68) {
+			if (distances[0] >= CHECK_DISTANCE) {
 				left90();
 				forwardOneCell(null);
-			} else if (distances[1] >= 1.68) {
+			} else if (distances[1] >= CHECK_DISTANCE) {
 				forwardOneCell(null);
-			} else if (distances[2] >= 1.68) {
+			} else if (distances[2] >= CHECK_DISTANCE) {
 				right90();
 				forwardOneCell(null);
 			} else { // it is trapped in a box
@@ -94,9 +121,7 @@ public class WalkForwardUntilDist {
 
 		}
 
-		if (!testing) {
-			System.out.println("Found the goal!");
-		}
+		System.out.println("Found the goal!");
 	}
 
 	/**
@@ -196,48 +221,52 @@ public class WalkForwardUntilDist {
 	private static void debug_checkDistances() {
 		Map<Character, Float> distances = new HashMap<Character, Float>();
 
-		// set distance sensor forward
-		TOP_MOTOR.rotateTo(0);
-		sampleProvider.fetchSample(samples, 0);
-		distances.put('N', samples[0]);
-		write("Forward distance: " + distances.get('N'));
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException err) {
-			err.printStackTrace();
-		}
+		while (true) { // repeat infinitely
 
-		// set distance sensor left
-		TOP_MOTOR.rotate(90);
-		sampleProvider.fetchSample(samples, 0);
-		distances.put('E', samples[0]);
-		write("East distance: " + distances.get('E'));
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException err) {
-			err.printStackTrace();
-		}
+			// set distance sensor forward
+			TOP_MOTOR.rotateTo(0);
+			sampleProvider.fetchSample(samples, 0);
+			distances.put('N', samples[0]);
+			write("Forward distance: " + distances.get('N'));
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException err) {
+				err.printStackTrace();
+			}
 
-		// set distance sensor right
-		TOP_MOTOR.rotate(-180);
-		sampleProvider.fetchSample(samples, 0);
-		distances.put('W', samples[0]);
-		write("West distance: " + distances.get('W'));
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException err) {
-			err.printStackTrace();
-		}
+			// set distance sensor left
+			TOP_MOTOR.rotate(90);
+			sampleProvider.fetchSample(samples, 0);
+			distances.put('E', samples[0]);
+			write("East distance: " + distances.get('E'));
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException err) {
+				err.printStackTrace();
+			}
 
-		// set distance sensor back
-		TOP_MOTOR.rotate(-90);
-		sampleProvider.fetchSample(samples, 0);
-		distances.put('S', samples[0]);
-		write("South distance: " + distances.get('S'));
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException err) {
-			err.printStackTrace();
+			// set distance sensor right
+			TOP_MOTOR.rotate(-180);
+			sampleProvider.fetchSample(samples, 0);
+			distances.put('W', samples[0]);
+			write("West distance: " + distances.get('W'));
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException err) {
+				err.printStackTrace();
+			}
+
+			// set distance sensor back
+			TOP_MOTOR.rotate(-90);
+			sampleProvider.fetchSample(samples, 0);
+			distances.put('S', samples[0]);
+			write("South distance: " + distances.get('S'));
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException err) {
+				err.printStackTrace();
+			}
+
 		}
 	}
 
@@ -272,21 +301,21 @@ public class WalkForwardUntilDist {
 	 * Sets up the motors for use
 	 */
 	private static void setUpMotors() {
-		robot = new DifferentialPilot(WHEEL_DIAMETER, TRACK_WIDTH, LEFT_MOTOR, RIGHT_MOTOR);
+		robot = new DifferentialPilot(WHEEL_DIAMETER, TRACK_WIDTH, LEFT_MOTOR, RIGHT_MOTOR, false);
 		robot.setTravelSpeed(TRAVEL_SPEED);
 		robot.setRotateSpeed(ROTATE_SPEED);
 
 		write("DifferentialPilot and motors initialized.");
 	}
 
-	/**
-	 * Sets up the bumper for use
-	 */
-	private static void setUpBumper() {
-		bumper = new EV3TouchSensor(SensorPort.S2);
-
-		write("Bumper initialized.");
-	}
+	// /**
+	// * Sets up the bumper for use
+	// */
+	// private static void setUpBumper() {
+	// bumper = new EV3TouchSensor(SensorPort.S2);
+	//
+	// write("Bumper initialized.");
+	// }
 
 	/**
 	 * Sets up the Ultrasonic sensor for use
@@ -298,13 +327,13 @@ public class WalkForwardUntilDist {
 		sampleProvider = distSense.getDistanceMode();
 
 		samples = new float[sampleProvider.sampleSize()];
-		
+
 		write("Distance sensor initialized.");
 	}
 
 	private static void setUpColorSensor() {
 		colorSense = new EV3ColorSensor(SensorPort.S4);
-		
+
 		write("Color sensor initialized.");
 	}
 
@@ -333,18 +362,38 @@ public class WalkForwardUntilDist {
 		screen.drawString(message, sWidth / 2, sHeight / 2, GraphicsLCD.BASELINE | GraphicsLCD.HCENTER);
 	}
 
-	private static void write(float message) {
-		write("" + message);
-	}
-
-	private static void write(int message) {
-		write("" + message);
-	}
-
+	/**
+	 * Convenience method to accept numbers and print them out in the write()
+	 * method.
+	 * 
+	 * @param message,
+	 *            a double value to print to the screen. Will also accept int,
+	 *            long, short, byte, or float
+	 */
 	private static void write(double message) {
 		write("" + message);
 	}
-	
+
+	/**
+	 * Shows a countdown on the screen.
+	 * 
+	 * @param startTime,
+	 *            how many units of time to wait, uses absolute value of this
+	 *            time
+	 * @param timeToPause,
+	 *            the length in ms of the unit of time (e.g. 1000 ms for one
+	 *            second), uses absolute value of this value entered, and a
+	 *            short data type ensures it cannot wait longer than ~32 seconds
+	 * @throws InterruptedException,
+	 *             if the notify() method is called during waiting
+	 */
+	private static void showCountdown(int startTime, short timeToPause) throws InterruptedException {
+		for (int i = Math.abs(startTime); i >= 0; i--) {
+			write(i);
+			Thread.sleep(Math.abs(timeToPause));
+		}
+	}
+
 	/*
 	 * 
 	 * 
@@ -357,7 +406,11 @@ public class WalkForwardUntilDist {
 	 * 
 	 * 
 	 */
-	
+
+	/**
+	 * The main thread which starts all accessories, started from the main()
+	 * method
+	 */
 	Thread main = new Thread() {
 		@Override
 		public void run() {
@@ -365,21 +418,28 @@ public class WalkForwardUntilDist {
 
 			setUpMotors();
 
-			setUpBumper();
+			// setUpBumper();
 
 			setUpDistanceSensor();
 
 			setUpColorSensor();
-			
-			solve(false);
+
+			startProgram(false);
 		}
 	};
-	
+
+	/**
+	 * The thread which allows us to stop at any point by pressing the ESCAPE
+	 * button. Started from the final method in the main thread.
+	 */
 	Thread stopper = new Thread() {
 		@Override
 		public void run() {
 			while (true) {
-				if (Button.ENTER.isDown()) {
+
+				Button.waitForAnyPress();
+
+				if (Button.ESCAPE.isDown()) {
 					System.exit(0);
 				}
 			}
